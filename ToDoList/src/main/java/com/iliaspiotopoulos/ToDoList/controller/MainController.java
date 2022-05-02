ae9eb01ea5d95aaf26a04dd235869d431bc70ff7
ilias000 <iliaspiot@yahoo.gr>
2022-05-02 13:47:10 +0300

    New functionality (Add task, Delete task, Get all tasks ordered by priority).

--- ToDoList/src/main/java/com/iliaspiotopoulos/ToDoList/controller/MainController.java
+++ ToDoList/src/main/java/com/iliaspiotopoulos/ToDoList/controller/MainController.java
@@ -1,12 +1,16 @@
 package com.iliaspiotopoulos.ToDoList.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import com.iliaspiotopoulos.ToDoList.model.Task;
 import com.iliaspiotopoulos.ToDoList.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,9 +23,35 @@ public class MainController
     public String getTasks(Model model)
     {
         List<Task> tasks = taskRepository.findAll();
-
         model.addAttribute("tasks", tasks); // In order to pass the list to the vew page (src/main/resources/templates/index.ftlh).
-
         return "index"; // In order to return the vew page (src/main/resources/templates/index.ftlh) you have to return only the file name.
+    }
+
+    @RequestMapping("/getTasksOrderByPriority")
+    public String getTasksOrderByPriority(Model model)
+    {
+        List<Task> tasks = taskRepository.findAllByOrderByPriority();
+        model.addAttribute("tasks", tasks); // In order to pass the list to the vew page (src/main/resources/templates/index.ftlh).
+        return "index"; // In order to return the vew page (src/main/resources/templates/index.ftlh) you have to return only the file name.
+    }
+
+    @RequestMapping("/done/{id}")
+    public String closeTask(@PathVariable UUID id)
+    {
+        taskRepository.deleteById(id); // Deleting the Task with the specific id.
+        return "redirect:/"; // Redirecting to the home page.
+    }
+
+    @RequestMapping("/newTaskForm")
+    public String getTaskForm()
+    {
+        return "task-form"; // Redirecting you to the page with the form to create a new task.
+    }
+
+    @PostMapping("/addTask")
+    public String addTask(@ModelAttribute Task task)
+    {
+        taskRepository.save(task); // Adding the task.
+        return "redirect:/"; // Redirecting to the home page.
     }
 }
--- ToDoList/src/main/java/com/iliaspiotopoulos/ToDoList/repository/TaskRepository.java
+++ ToDoList/src/main/java/com/iliaspiotopoulos/ToDoList/repository/TaskRepository.java
@@ -1,5 +1,6 @@
 package com.iliaspiotopoulos.ToDoList.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.iliaspiotopoulos.ToDoList.model.Task;
@@ -7,4 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, UUID> {}
+public interface TaskRepository extends JpaRepository<Task, UUID>
+{
+    List<Task> findAllByOrderByPriority();
+}
