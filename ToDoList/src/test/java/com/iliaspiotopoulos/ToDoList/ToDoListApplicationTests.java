c795c065e651dc1264a921c4d7805ad51e2ce467
ilias000 <iliaspiot@yahoo.gr>
2022-05-02 18:41:40 +0300

    ToDoListApplicationTests.

--- ToDoList/src/test/java/com/iliaspiotopoulos/ToDoList/ToDoListApplicationTests.java
+++ ToDoList/src/test/java/com/iliaspiotopoulos/ToDoList/ToDoListApplicationTests.java
@@ -3,11 +3,15 @@ package com.iliaspiotopoulos.ToDoList;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest
 class ToDoListApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads()
+	{
+		assertDoesNotThrow(() -> ToDoListApplication.main(new String[]{}));
 	}
 
 }
